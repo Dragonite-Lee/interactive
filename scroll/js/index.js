c9eaf798332f14d4184ddc7d36a764232870985b
dragonite-lee <ross1222@naver.com>
2023-08-29 21:28:38 +0900

    페럴럭스 중간정도

--- scroll/js/index.js
+++ scroll/js/index.js
@@ -1,7 +1,12 @@
 const h1 = document.querySelector('h1');
+const progressBar = document.querySelector('.bar');
+const submarine = document.querySelector('.submarine');
 // h1.innerText = "1";
 let scrollNum = 0;
 window.addEventListener("scroll", () => {
     scrollNum = window.scrollY;
-    h1.innerText = ((scrollNum / (document.body.scrollHeight - window.innerHeight)) * 100).toFixed(0) + "%";
+    h1.innerText = scrollNum.toFixed(0) + "m";
+    progressBar.style.width = ((scrollNum / (document.body.scrollHeight - window.innerHeight)) * 100).toFixed(0) + "%";
+
+    submarine.style.transform = `translateX(${((scrollNum / window.innerWidth) * 100).toFixed(0)}%)`;
 })
