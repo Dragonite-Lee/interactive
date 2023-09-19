68d25c04ab26951877599fe83427e6a8d6629344
dragonite-lee <ross1222@naver.com>
2023-09-19 10:32:11 +0900

    cd플레이어 완료

--- cd/js/index.js
+++ cd/js/index.js
@@ -37,6 +37,62 @@ window.onload = function() {
         })(i);
     }
     
+    if(mobileChk()) {
+        wrap.addEventListener("touchstart", touchFunc, false);
+        // wrap.addEventListener("touchmove", touchFunc, false);
+        wrap.addEventListener("touchend", touchFunc, false);
+    }
+
+    let start_X = 0;
+    let end_X = 0;
+    let type = null;
+    let touch = null;
+
+    function touchFunc(evt) {
+        // evt.type을 하면 touchstart touchend 두 개의 상태만 가져오게 됨
+        switch (evt.type) {
+            case "touchstart": //touchstart에선 터치를 시작하는 위치만 기억하면 됨
+                type = "mousedown";
+                touch = evt.changedTouches[0];
+                start_X = touch.clientX; //화면 왼쪽부터 터치한 곳까지의 값이 나오게 됨 
+                end_X = 0;
+            break;
+            // case "touchmove":
+            //     type = "mousemove";
+            //     touch = evt.changedTouches[0];
+            //     console.log(touch)
+            // break;
+            case "touchend": //touchend에서도 터치를 어디서 땠는지를 기억해야 함
+                type = "mouseup";
+                touch = evt.changedTouches[0];
+                end_X = touch.clientX;
+
+                let chkNum = start_X - end_X;
+                let chkNumAbs = Math.abs(chkNum); //방향말고 얼만큼 움직였는지 알아야 할 때
+
+                if (chkNumAbs > 100) {
+                    //터치를 많이해서 넘어가야 하는 상황
+                    if (chkNum < 0) {
+                        //왼쪽으로 터치
+                        if (pageNum > 0) {
+                            pageNum--;
+                        } else {
+                            pageNum = totalNum - 1;
+                        }
+                    } else {
+                        //오른쪽으로 터치
+                        if (pageNum < totalNum - 1) {
+                            pageNum++;
+                        } else {
+                            pageNum = 0;
+                        }
+                    }
+                    pageChangeFunc();
+                }
+            break;
+        }
+    }
+
     pageChangeFunc();
 }
 
@@ -53,4 +109,14 @@ function pageChangeFunc() {
         }
     }
     console.log(pageNum)
+}
+
+function mobileChk() {
+    var mobileKeyWords = new Array('Android', 'iPhone', 'iPod', 'BlackBerry', 'Windows CE', 'SAMSUNG', 'LG', 'MOT', 'SonyEricsson');
+    for (var info in mobileKeyWords) {
+        if (navigator.userAgent.match(mobileKeyWords[info]) != null) {
+            return true;
+        }
+    }
+    return false;
 }
